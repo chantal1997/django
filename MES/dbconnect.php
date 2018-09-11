3b0b591227d90ce7703c0d763f948ee7ab9eb9c1
chantal1997 <chantal.oganga@strathmore.edu>
2018-09-11 19:47:39 +0300

    First commit

---
+++ MES/dbconnect.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost3306";
+$username = "root";
+$database = "project";
+$password = "anita";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+?>
